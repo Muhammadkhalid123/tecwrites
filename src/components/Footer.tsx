30d97214f574f9d70b657f4afe4e3b715c2b41bd
Muhammadkhalid123 <khalid8sharpk@gmail.com>
2026-07-30 18:02:04 -0700

    info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -97,7 +97,7 @@ export default function Footer() {
                                 >
                                     <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
                                 </svg>
-                                +92 3391252535
+                                +1(888)921 3331
                             </li>
                             <li className="flex items-start gap-3">
                                 <svg
@@ -131,7 +131,7 @@ export default function Footer() {
                                     <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
                                     <circle cx="12" cy="10" r="3" />
                                 </svg>
-                                <span>Al Naseer Tower 3, Gulistan e Johar Block 1, Main University Road, Karachi</span>
+                                <span>31 Gourdon Ct, Lake St. Louis, MO 63367</span>
                             </li>
                         </ul>
                         <div className="flex gap-4 mt-6">
